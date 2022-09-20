3fda49b2d29c99f7189b91a143dfa552733d8683
Jimmy Li <lijjimmy@gmail.com>
2022-09-19 23:39:56 -0400

    changed text of button depending on current theme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 document.querySelector('.toggle-theme-button').addEventListener('click', () =>{
     document.body.classList.toggle('dark')
+    let button = document.querySelector('.toggle-theme-button')
+    console.log(button.innerHTML)
+    button.innerHTML === "Dark Mode" ? button.innerHTML = "Light Mode" : button.innerHTML = "Dark Mode"
 })
